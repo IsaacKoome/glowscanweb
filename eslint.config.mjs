8bbfa351d7c7b5d13fb4dd679d14f4ac20b578c2
komecon <128533780+IsaacKoome@users.noreply.github.com>
2025-07-25 15:00:30 +0300

    few fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,16 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // ✅ Add custom rule overrides here
-  {
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "react/no-unescaped-entities": "off",
-      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
