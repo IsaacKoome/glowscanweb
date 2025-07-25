d1add8eac12c77c87c6fe406ed821d9910a582a6
komecon <128533780+IsaacKoome@users.noreply.github.com>
2025-07-25 15:04:00 +0300

    new fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
     extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   }),
 ];
